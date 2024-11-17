09538114de067e1c46f22f83e6bb4a0f78c042f8
@KARAMOKO-sys <detoleandre@gmail>
2024-11-17 08:03:55 +0000

    Feature structurexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

---
+++ back-end-swap-food/src/main/java/com/swap/food/BackEndSwapFoodApplication.java
@@ -0,0 +1,13 @@
+package com.swap.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSwapFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndSwapFoodApplication.class, args);
+	}
+
+}
